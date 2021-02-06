893f05f927f7138870608adfe2520f4fecf8a694
AlexanderSvobodskii <72270748+AlexanderSvobodskii@users.noreply.github.com>
2021-02-06 19:35:00 +0300

    Update Parsing.py

--- Parsing.py
+++ Parsing.py
@@ -1,16 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 import pandas as pd
 import matplotlib as mpl
 import re
 import os
 
-
-# In[14]:
 
 
 path = 'C:\\Users\\Компьютер\\Desktop\\test_schedule.inc'
@@ -24,9 +16,6 @@ if (ident == True):
     f.close()
 else :
     print('File does not exist in such directory')
-
-
-# In[ ]:
 
 
 
