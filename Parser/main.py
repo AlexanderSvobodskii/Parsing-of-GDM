689265530b1cd377d9a10aa638f3114ac02f8db2
Alexander Swobodskii <kalyuzhin.alexander@gmail.com>
2021-03-11 17:27:39 +0300

    Модифицированы функции, добавлен вывод в csv

--- Parser/main.py
+++ Parser/main.py
@@ -12,28 +12,20 @@ if __name__ == "__main__":
     # If this file was imported to another files, this code will be ignored
     
     input_file = 'input_data_test_schedule.inc'
-    output_file = ''
+    output_file = 'output_data_schedule.csv'
     
     # checking of existing of .inc file in given path
     ident = os.path.exists(input_file) 
     
-    keywords = ('DATES', 'COMPDAT', 'COMPDATL')
-    parameters = ("Date", "Well name", "Local grid name", "I", "J", "K upper", 
+    keywords = ('DATES', 'COMPDAT', 'COMPDATL', 'END')
+    parameters = ("Well name", "Local grid name", "I", "J", "K upper", 
                   "K lower", "Flag of connection", "Saturation table", 
                   "Transmissibility", 
-                  "Well bore diameter", "Effective Kh", "Skin factor", "D_factor")
+                  "Well bore diameter", "Effective Kh", "Skin factor", "D_factor", "Well orientation", "Effective radius")
 
     if (ident == True):
         
-        input_file_list = []
-        f = open(input_file, 'r')
-        for line in f:
-            input_file_list.append(line)
-        str_input_file = sp.clean_schedule(input_file_list)
-        
-        #print(sp.parse_keyword_DATE_line(str_input_file))
-        #print(sp.parse_keyword_COMPDATL_line("'W3' 'LGR1' 10 10  2   2 	OPEN 	1* 	1	2 	1 	3* 			1.0918 /"))
-        #print(sp.parse_keyword_COMPDAT_line("'W1' 10 10 1 3 OPEN 1* 1 2 1 3* 1.0 /"))
-        #print(sp.default_params_unpacking_in_line("'W1' 10 10 1 3 OPEN 1* 1 2 1 3* 1.0 /"))
+        sp.to_csv(input_file, output_file, parameters, keywords)
+
     else :
         print('File does not exist in such directory')
--- Parser/schedule_parser.py
+++ Parser/schedule_parser.py
@@ -4,21 +4,48 @@ import numpy as np
 import re
 import os
 
-def to_csv(input_file, output_file, parameters, keywords):
+def to_csv(input_file: str, output_file: str, parameters: tuple, keywords: tuple):
     
     # this function transform .inc-file to .csv file
     
+    input_file_list = []
+    f = open(input_file, 'r')
+    for line in f:
+        input_file_list.append(line)
+    clean_file_list = clean_schedule(input_file_list)
+        
+    edit_file_list = edit_schedule(clean_file_list, keywords)
     
-    return
-
-def read_schedule():
-    # reading of shedule file
+    blocks = extract_keyword_blocks(edit_file_list)
     
+    data_list = []
+    buff = []
     
-    return
-
-def inspection():
-    # inspection of correctness of fullness of .inc file
+    for i in blocks.keys():
+        k = len(blocks[i])
+        for j in range(k):
+            blocks[i][j].insert(0, i)
+            buff.append(blocks[i][j])
+        data_list.append(buff)
+        buff = []
+    
+    data_final = []
+    for i in range(len(data_list)):
+        k = len(data_list[i])
+        for j in range(k):
+            data_final.append(data_list[i][j])
+            
+    dates = []
+    well_data = []
+    
+    for i in range(len(data_final)):
+        dates.append(data_final[i][0])
+        well_data.append(data_final[i][1:])
+        
+    df = pd.DataFrame(well_data, index = dates, columns=parameters)
+    
+    df.to_csv(output_file)
+    
     return
 
 def clean_schedule(input_file_list: list):
@@ -44,34 +71,139 @@ def clean_schedule(input_file_list: list):
             s1[i] = s1[i] + '\n' 
         if (s1[i] != '\n'):
                 s2.append(s1[i])
+                
+    return s2
+
+def edit_schedule(clean_file_list: list, keywords: tuple):
     
-    ret_str = ''
-    for i in range(len(s2)):
-        ret_str += s2[i]
+    # function which edits uncommented schedule inc.-file
+    #----------------------cleaning from excess keywords------------------
+    i = 0
+    while True:
+        
+        j = 0
+        
+        if (i == 0) and (clean_file_list[i][:clean_file_list[i].find("'\n'")] not in keywords):
+            while (clean_file_list[i][:clean_file_list[i].find("'\n'")] not in keywords):
+                clean_file_list.pop(i)
+                
+        if ((i > 0) and (clean_file_list[i] == '/\n') and (clean_file_list[i+1][:clean_file_list[i+1].find("'\n'")] not in keywords)):
+            j = i+1
+            while (clean_file_list[j][:clean_file_list[j].find("'\n'")] not in keywords):
+                clean_file_list.pop(j)               
+        
+        if (clean_file_list[i] == 'END\n'):
+            clean_file_list[i] = 'END'
+            break
+        i += 1
+    #------------------------------------------------------------------------
     
-    return ret_str
-
-def parser_schedule():
-
-    return
-
-def extract_keyword_blocks():
-    # extracting of keyword blocks from .inc-file
+    #--------------------cleaning from excess dates--------------------------
+    i = 0
+    while True:
+        
+        j = 0
+        if (clean_file_list[i] == 'DATES\n'):
+            j = i+1
+            while (clean_file_list[j+1] != '/\n'):
+                if (clean_file_list[j+1] != '/\n'):
+                    clean_file_list.pop(j)
+        
+        if (clean_file_list[i] == 'END'):
+            break
+        i += 1
+    #------------------------------------------------------------------------
     
-    return
-
-def extract_lines():
-    # extracting of string of .inc-file from blocks
-    return
-
-def parse_keyword_block():
+    #ret_str = ''
+    #for i in range(len(clean_file_list)):
+        #ret_str += clean_file_list[i]
     
-    return
+    #return ret_str
+    return clean_file_list
+
+def extract_keyword_blocks(clean_file_list: list):
+    # extracting of keyword blocks from .inc-file and preparing information to writing to csv-file
+    
+    for i in range(len(clean_file_list)):
+        if (clean_file_list[i] == 'COMPDAT\n'):
+            j = i+1
+            while (clean_file_list[j] != '/\n'):
+                clean_file_list[j] = parse_keyword_COMPDAT_line(clean_file_list[j])
+                j+=1
+        if (clean_file_list[i] == 'COMPDATL\n'):
+            j = i+1
+            while (clean_file_list[j] != '/\n'):
+                clean_file_list[j] = parse_keyword_COMPDATL_line(clean_file_list[j])
+                j+=1
+    
+    if (clean_file_list[0] == 'DATES\n'):
+        
+        data_dict = {}
+        dates = ''
+        data = []
+        
+        for j in range(0, len(clean_file_list), 1):
+            if (clean_file_list[j] == 'DATES\n'):
+                dates = parse_keyword_DATE_line(clean_file_list[j+1])
+                k=j+2
+                while (clean_file_list[k] != 'DATES\n'):
+                    if ((clean_file_list[k] != 'COMPDAT\n') and (clean_file_list[k] != 'COMPDATL\n') and (clean_file_list[k] != '/\n')):
+                        data.append(clean_file_list[k])
+                   
+                    k+=1              
+                
+                    if clean_file_list[k] == 'END':
+                        break
+                data_dict[dates] = data
+                data = []
+                dates = ''
+                
+        for comp_wells in data_dict.keys():
+            if (data_dict[comp_wells] == []):
+                data_dict[comp_wells] = [[np.nan for r in range(15)]]
+        
+    if (clean_file_list[0] != 'DATES\n'):
+        
+        data_dict = {}
+        dates = ''
+        data = []
+        
+        dates = 'INITIAL DATE'
+        i = 0
+        while (clean_file_list[i] != 'DATES\n'):
+               if ((clean_file_list[i] != 'COMPDAT\n') and (clean_file_list[i] != 'COMPDATL\n') and (clean_file_list[i] != '/\n')):
+                   data.append(clean_file_list[i])
+               i+=1
+        data_dict[dates] = data
+        data = []
+        dates = ''
+        
+        for j in range(i, len(clean_file_list), 1):
+            if (clean_file_list[j] == 'DATES\n'):
+                dates = parse_keyword_DATE_line(clean_file_list[j+1])
+                k=j+2
+                while (clean_file_list[k] != 'DATES\n'):
+                    if ((clean_file_list[k] != 'COMPDAT\n') and (clean_file_list[k] != 'COMPDATL\n') and (clean_file_list[k] != '/\n')):
+                        data.append(clean_file_list[k]) 
+                   
+                    k+=1              
+                
+                    if clean_file_list[k] == 'END':
+                        break
+                data_dict[dates] = data
+                data = []
+                dates = ''
+        
+        for comp_wells in data_dict.keys():
+            if (data_dict[comp_wells] == []):
+                data_dict[comp_wells] = [[np.nan for r in range(15)]]
+            
+    return data_dict
 
 def parse_keyword_DATE_line(current_date_line: str):
     
     format_date = r'\d{2}\s+\w{3}\s+\d{4}' # regular expression for date
-    string = re.findall(format_date, current_date_line) 
+    string = re.findall(format_date, current_date_line)
     
     return string[0]
 
@@ -96,8 +228,6 @@ def parse_keyword_COMPDATL_line(well_comp_line: str):
         well_comp_line_1.append(well_array[i])
     
     return well_comp_line_1
-    
-    #return well_comp_line
 
 def parse_keyword_COMPDAT_line(well_comp_line: str):
     
